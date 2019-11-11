30fcef3acc92bf8174e1683d21bea307af83687f
Kathy Zhang <1316646377@qq.com>
2019-11-11 13:24:41 +0800

    React 动画 sample

--- src/App.js
+++ src/App.js
@@ -1,39 +1,76 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        Hello World
-      </div>
-    )
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            // show: true
+            list: []
+        };
+        this.handleAddItem = this.handleAddItem.bind(this);
+        this.handleToggle = this.handleToggle.bind(this);
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <TransitionGroup>
+                {
+                    this.state.list.map((item, index) => {
+                        return (
+                            <CSSTransition
+                                timeout = {1000}
+                                classNames = 'fade'
+                                unmountOnExit = {true}
+                                onEntered = {(el) => {el.style.color = 'blue'}}
+                                appear = {true}
+                                key = {index}>
+
+                                <div>{item}</div>
+                            </CSSTransition>
+                        )
+                    })
+                }
+                </TransitionGroup>
+                <button onClick = {this.handleAddItem}>toggle</button> 
+
+                {/* <CSSTransition
+                    in = {this.state.show}
+                    timeout = {1000}
+                    classNames = 'fade'
+                    unmountOnExit = 'true'
+                    // 钩子函数的el参数代表包裹的div元素
+                    onEntered = {(el) => {el.style.color = 'blue'}}
+                    // appear 首次加载页面的时候也需要动画效果，会自动给元素添加fade-appear, fade-appear-active, fade-appear-done的class
+                    appear = 'true'>
+                    <div>hello</div>
+                </CSSTransition>
+                <button onClick = {this.handleToggle}>toggle</button>  */}
+
+                {/* 
+                    <div className = {this.state.show ? 'show' : 'hide'}>hello</div>
+                    <button onClick = {this.handleToggle}>toggle</button> 
+                */}
+            </Fragment>
+        )
+    }
+
+    handleAddItem() {
+        this.setState((prevState) => {
+            return {
+                list: [...this.state.list, 'item']
+            }
+        });
+    }
+
+    handleToggle() {
+        this.setState({
+            show: this.state.show ? false : true
+        });
+    }
 }
 
 export default App;
